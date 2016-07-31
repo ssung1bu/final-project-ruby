5be9c96676da2c95226c94db251c4c9eebeef44d
Sarah Sung <mssarahsung@gmail.com>
2016-07-31 15:12:04 -0400

    init commit

---
+++ db/migrate/20160731185611_add_avatars_to_users.rb
@@ -0,0 +1,13 @@
+class AddAvatarsToUsers < ActiveRecord::Migration
+  
+  def self.up
+    change_table :posts do |t|
+      t.attachment :avatar
+    end
+  end
+
+  def self.down
+    remove_attachment :posts, :avatar
+  end
+
+end
