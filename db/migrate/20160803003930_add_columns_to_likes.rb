38329f28330f025fd278edba7d4e7afe2630d324
Sarah <mssarahsung@gmail.com>
2016-08-03 18:51:33 -0400

    add acts_as_votable gem

--- db/migrate/20160803003930_add_columns_to_likes.rb
+++
@@ -1,6 +0,0 @@
-class AddColumnsToLikes < ActiveRecord::Migration
-  def change
-    add_column :likes, :liking_user, :references 
-    add_column :likes, :liked_user, :references
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160731210126) do
+ActiveRecord::Schema.define(version: 20160803011209) do
 
   create_table "follows", force: :cascade do |t|
     t.integer  "following_user"
     t.integer  "followed_user"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "links", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "posts", force: :cascade do |t|
@@ -48,5 +58,20 @@ ActiveRecord::Schema.define(version: 20160731210126) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+
+  create_table "votes", force: :cascade do |t|
+    t.integer  "votable_id"
+    t.string   "votable_type"
+    t.integer  "voter_id"
+    t.string   "voter_type"
+    t.boolean  "vote_flag"
+    t.string   "vote_scope"
+    t.integer  "vote_weight"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "votes", ["votable_id", "votable_type", "vote_scope"], name: "index_votes_on_votable_id_and_votable_type_and_vote_scope"
+  add_index "votes", ["voter_id", "voter_type", "vote_scope"], name: "index_votes_on_voter_id_and_voter_type_and_vote_scope"
 
 end
